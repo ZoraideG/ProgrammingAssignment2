f19dd6edefae5489a058e2f460b4228150d45d4b
ZoraideG <zoraide.granchi@gmail.com>
2014-05-25 18:47:19 +0200

    Zoraides version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair function allows to skip the calculation of 
+##a matrix inverse if this is already been calculated into the same 
+##environment. 
+## the first function creates a list of function containing the matrix input and the 
+##previously calculated inverse. 
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(invers) inv <<- invers
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## the following function checks whether the inverse of the given matrix
+## already exists. If it exists, it returns the inverse matrix, 
+##otherwise it performs the calculation. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        else{
+                print("calculating inverse")
+                data <- x$get()
+                inv <- solve(data, ...)
+                x$setinv(inv)       
+        }
+        
+        inv
 }
+
+
